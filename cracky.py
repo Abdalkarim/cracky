558e914b61f9055a26b5f67790a45a86304267c8
Mahmoud Abd Alkarim <abdalkarim.mail@gmail.com>
2021-07-27 02:05:51 +0200

    Add files via upload

---
+++ cracky.py
@@ -0,0 +1,43 @@
+import PyPDF2,sys
+
+print("""
+     	  __   __   ___     
+     	 |ma) |ak\ |__      
+         |    |  / |        
+                         
+	 __        __   __       
+	/  `  /\  |th) /on` |__/ 
+	\__, /~~\ |  \ \__, |  \ @h0x72us
+                         
+"""
+)
+if len(sys.argv) == 3:
+	fileName = sys.argv[1]
+	passFile = sys.argv[2]
+else:
+	print("[*] Usage python3 cracky.py pdfFile.pdf wordlist.txt\n")
+	sys.exit()
+
+def crack():
+
+	pdfFile = open(fileName,'rb')
+	pdfObj  = PyPDF2.PdfFileReader(pdfFile)
+	
+	if pdfObj.isEncrypted:
+		with open(passFile,'rb') as f:
+			lines = f.readlines()
+			for line in lines:
+				line = line.strip()
+				try:
+					pdfObj.decrypt(line)
+					print("[*] Password found : {}\n".format(str(line,'utf-8')))
+					break
+				except:
+					continue
+			print("[*] Password not found in your wordlist try another wordlist.\n")
+	else:
+		print("[*] your pdf file not encrypted.")		
+		
+
+if __name__ == '__main__':
+	crack()
